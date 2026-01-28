68eea958e3865cd6639233b58975c5d376ebd125
Juraj Giacko <juraj@giacko.sk>
2026-01-29 00:18:58 +0100

    feat: increase logo size in QR code for better visibility

--- src/components/qr-generator.tsx
+++ src/components/qr-generator.tsx
@@ -78,8 +78,8 @@ export function QRGenerator({ onSave }: QRGeneratorProps) {
       },
       imageOptions: {
         crossOrigin: 'anonymous',
-        margin: 10,
-        imageSize: 0.4,
+        margin: 5,
+        imageSize: 0.5,
       },
       cornersSquareOptions: {
         type: 'extra-rounded',
